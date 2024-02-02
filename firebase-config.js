85c73b8eaa7b4e72f77296effc5c2f7fc4335a70
Vinybsqn <vianneybasquin59@gmail.com>
2024-02-02 11:18:40 +0100

    firebase connection

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAWbvcqrKYJcymzXD1MjeyL7jabbJ6fwfs",
+    authDomain: "echecmatch-ae7c7.firebaseapp.com",
+    projectId: "echecmatch-ae7c7",
+    storageBucket: "echecmatch-ae7c7.appspot.com",
+    messagingSenderId: "300684718853",
+    appId: "1:300684718853:web:3570b481256dd391c67d6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
