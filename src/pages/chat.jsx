f566dec1bcff0714e618e145a988ad99107d140c
Vinybsqn <vianneybasquin59@gmail.com>
2024-02-23 16:04:47 +0100

    test sockets

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,37 +1,52 @@
-// ChatPage.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import '../styles/chatPage.css'; // Assurez-vous que le chemin est correct
 
+// Établir une connexion socket en dehors du composant pour éviter des reconnexions à chaque re-render
+const socket = io('http://localhost:5173/chat'); // Remplacez 'http://localhost:3001' par l'URL de votre serveur
+
 const ChatPage = () => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!message.trim()) return;
-    setMessages([...messages, message]);
-    setMessage('');
-  };
+    useEffect(() => {
+        // Écouter les messages entrants
+        socket.on('chat message', (msg) => {
+            setMessages((msgs) => [...msgs, msg]);
+        });
 
-  return (
-    <div className="chat-container">
-      <div className="messages-container">
-        {messages.map((msg, index) => (
-          <p key={index} className="message">{msg}</p>
-        ))}
-      </div>
-      <form onSubmit={handleSubmit} className="message-form">
-        <input
-          type="text"
-          className="message-input"
-          placeholder="Type your message..."
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button type="submit" className="send-button">Send</button>
-      </form>
-    </div>
-  );
+        // Nettoyer l'effet en se désabonnant de l'événement
+        return () => {
+            socket.off('chat message');
+        };
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!message.trim()) return;
+        socket.emit('chat message', message); // Envoyer le message au serveur
+        setMessage('');
+    };
+
+    return (
+        <div className="chat-container">
+            <div className="messages-container">
+                {messages.map((msg, index) => (
+                    <p key={index} className="message">{msg}</p>
+                ))}
+            </div>
+            <form onSubmit={handleSubmit} className="message-form">
+                <input
+                    type="text"
+                    className="message-input"
+                    placeholder="Tapez votre message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button type="submit" className="send-button">Envoyer</button>
+            </form>
+        </div>
+    );
 };
 
 export default ChatPage;
