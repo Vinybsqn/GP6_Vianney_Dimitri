3c7811b5866fbddb5ac111681cc9fbe848b6d667
Vinybsqn <vianneybasquin59@gmail.com>
2024-04-27 00:05:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ function App() {
                         <PrivateRoute>
                             <Nav />
                             <HomePage />
-                            <AfficherUtilisateur />
                         </PrivateRoute>
                     }
                 />
